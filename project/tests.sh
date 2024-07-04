1c8933824d67a1e7446baaeedcfdfaaa0416053c
hendhussienfau <104495535+hendhussienfau@users.noreply.github.com>
2024-07-04 13:18:55 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -6,5 +6,10 @@ pip install sqlite3
 pip install tabulate
 pip install sqlalchemy
 pip install os
+pip install scipy
+pip install seaborn
+pip install matplotlib
+pip install statsmodels
+pip install  numpy
 
 python3 ./project/pipeline_test.py
